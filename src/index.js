1fa95c8f87346f502bee066fbfee4d99ab46c59c
huxuezi <huxuezi@huohua.cn>
2019-04-03 19:31:36 +0800

    initial

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import dva from 'dva';
+import {createLogger} from 'redux-logger';
+import createLoading from 'dva-loading';
+import createHistory from 'history/createBrowserHistory';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export const browserHistory = createHistory();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// 1. Initialize
+const app = dva({
+  history: browserHistory,
+  onAction: createLogger(),
+});
+
+// 2. Plugins
+app.use(createLoading());
+
+// 3. Model
+app.model(require('./models').default);
+
+// 4. Router
+app.router(require('./router').default);
+
+// 5. Start
+app.start('#root');
